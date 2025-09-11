fc97aa742c803727c7fd6d8caa00c747e21e775e
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2025-09-11 10:29:32 +0300

    3_osa_kordused.cs

--- 3_osa_kordused.cs
+++ 3_osa_kordused.cs
@@ -55,3 +55,41 @@ public static Tuple<double, double, double> AnalüüsiArve(double[] arvud)
     }
     return Tuple.Create(summa, keskmine, korrutis);
 }
+
+//harjutus 3
+class Inimene
+{
+    public string Nimi { get; set; }
+    public int Vanus { get; set; }
+
+    public Inimene(string nimi, int vanus)
+    {
+        Nimi = nimi;
+        Vanus = vanus;
+    }
+
+    public static Tuple<int, double, Inimene, Inimene> Statistika(List<Inimene> inimesed)
+    {
+        int summa = 0;
+        foreach (var inimene in inimesed)
+        {
+            summa += inimene.Vanus;
+        }
+
+        double keskmine = (double)summa / inimesed.Count;
+
+        // Leia vanim ja noorim
+        Inimene vanim = inimesed[0];
+        Inimene noorim = inimesed[0];
+
+        foreach (var inimene in inimesed)
+        {
+            if (inimene.Vanus > vanim.Vanus)
+                vanim = inimene;
+            if (inimene.Vanus < noorim.Vanus)
+                noorim = inimene;
+        }
+
+        return Tuple.Create(summa, keskmine, vanim, noorim);
+    }
+}
