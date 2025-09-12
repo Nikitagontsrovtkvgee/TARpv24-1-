19d6724d3e07a412ec5a57ee504e39a1ae3c9dd0
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2025-09-12 11:40:32 +0300

    Toode.cs

---
+++ Toode.cs
@@ -0,0 +1,14 @@
+namespace TARpv24__1_
+{
+    public class Toode
+    {
+        public string Nimi { get; set; }
+        public double Kalorid100g { get; set; }
+
+        public Toode(string nimi, double kalorid100g)
+        {
+            Nimi = nimi;
+            Kalorid100g = kalorid100g;
+        }
+    }
+}
